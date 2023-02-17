b4a6e8fb38c60073d3349a998004067a8e7895e3
LucaBroeders <95628586+LucaBroeders@users.noreply.github.com>
2023-02-17 12:37:01 +0100

    login_validatie

---
+++ login_validatie.php
@@ -0,0 +1,33 @@
+<?php
+
+// Start a session
+session_start();
+
+// Check if the form has been submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Retrieve the username and password from the form data
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Validate the login credentials
+    if ($username === 'myusername' && $password === 'mypassword') {
+
+        // Login successful, redirect to the dashboard page
+        $_SESSION['username'] = $username;
+        header('Location: dashboard.php');
+        exit;
+
+    } else {
+
+        // Login failed, set an error message
+        $_SESSION['error'] = 'Invalid username or password';
+
+        // Redirect back to the login page
+        header('Location: login.php');
+        exit;
+
+    }
+}
+
+?>
